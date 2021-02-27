9618a7f953a1653222aad01257b619e7e7635d49
Poorna-A <apurna1998@gmail.com>
2021-02-27 11:23:15 +0530

    palindrome function

---
+++ palindrromefn.py
@@ -0,0 +1,17 @@
+def palindrome(a):
+   
+    rev=0
+    rem=0
+    temp=a
+    while(a != 0):
+        rem=a%10
+        rev=(rev*10)+rem
+        a=a//10
+    if(temp==rev):
+        x="num is palindrome"
+    else:
+        x="not palindrome"
+    return x
+a=int(input("enter a number"))
+x=palindrome(a)
+print(x)
